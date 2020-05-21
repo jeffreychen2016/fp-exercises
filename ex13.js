bf04895f5870e8ded1312c65c0bcc4aaf6d515df
Jeffrey Chen <jeffreychen@BNAJCHENMAC.local>
2020-05-20 20:46:38 -0500

    feat: add ex13

--- ex13.js
+++ ex13.js
@@ -6,3 +6,25 @@
 // U(x).map(f) === U(f(x))
 // map applies a function to the value(s) within a functor
 // ---------------------------------------
+
+const { tagged } = require("daggy");
+
+const Functor = tagged("Functor", ["value"]);
+
+// map :: Functor f => f a ~> (a -> b) -> f b
+Functor.prototype.map = function (fn) {
+  const result = [];
+  for (let i = 0; i < this.value.length; i++) {
+    result.push(fn(this.value[i]));
+  }
+
+  return Functor(result);
+};
+
+const array = Functor([1, 2, 3, 4]);
+
+const add1 = (x) => x + 1;
+const add2 = (x) => x + 2;
+
+console.log(array.map(add1).value); // return Functor type with value [2,3,4,5]
+console.log(array.map(add1).map(add2).value); // return Functor type with value [4,5,6,7]
